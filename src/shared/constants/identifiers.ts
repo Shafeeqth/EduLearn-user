210e0be6653b77c22911acd2425b0f4d0b9826dc
shafeeque <shafeeqsha06@gmail.com>
2025-03-27 21:06:32 +0530

    feat(constants): added few reusable constants

---
+++ src/shared/constants/identifiers.ts
@@ -0,0 +1,11 @@
+export const TYPES = {
+  IUserRepository: Symbol.for('IUserRepository'),
+  IHashService: Symbol.for('IHashService'),
+  IRegisterUserUseCase: Symbol.for('IRegisterUserUseCase'),
+  IUUIDService: Symbol.for('IUUIDService'),
+  IUserServiceController: Symbol.for('IUserServiceController'),
+  IRefreshTokenRepository: Symbol.for('IRefreshTokenRepository'),
+  ITokenService: Symbol.for('ITokenService'),
+  ILoginUserUseCase: Symbol.for('ILoginUserUseCase'),
+  INewRefreshTokenUseCase: Symbol.for('INewRefreshTokenUseCase'),
+};
