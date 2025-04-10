fc5cc38174b3954938166c5d7a2a77b08218f765
shafeeque <shafeeqsha06@gmail.com>
2025-04-10 23:52:09 +0530

    feat: added few types for user, refresh-token for better typesafty

---
+++ src/shared/types/user-roles.ts
@@ -0,0 +1,5 @@
+export enum UserRoles {
+  ADMIN = 'admin',
+  INSTRUCTOR = 'instructor',
+  USER = 'user',
+}
---
+++ src/shared/types/user-status.ts
@@ -0,0 +1,5 @@
+export enum UserStatus {
+  ACTIVE = 'active',
+  NOT_ACTIVE = 'not_active',
+  BLOCKED = 'blocked',
+}
