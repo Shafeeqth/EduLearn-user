d09264cdd5a4fb35b8db7de362883204fc10d28b
shafeeque <shafeeqsha06@gmail.com>
2025-04-21 22:53:38 +0530

    feat(verify-dto): added verify user dto to codebase

---
+++ src/application/dto/auth/verify-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail } from 'class-validator';
+import { IUser } from '@/domain/interfaces/user';
+
+export default class VerifyUserDto implements Partial<IUser> {
+  @IsEmail({}, { message: 'Invalid email format' })
+  email!: string;
+
+  userId?: string;
+
+  timeStamp?: string;
+}
