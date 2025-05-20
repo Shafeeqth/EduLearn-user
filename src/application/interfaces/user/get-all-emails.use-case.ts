4f365756e92644b084cc5901f7022de37b06d2f4
shafeeque <shafeeqsha06@gmail.com>
2025-05-20 13:20:18 +0530

    feat(all-emails): add interface and implemntaion for get all emails use case

---
+++ src/application/interfaces/user/get-all-emails.use-case.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Interface representing the use case to fetch all registered emails
+ */
+export interface IGetAllEmailsUseCase {
+  /**
+   * @returns A Promise resolve to `string[]`
+   */
+  execute(): Promise<string[]>;
+}
