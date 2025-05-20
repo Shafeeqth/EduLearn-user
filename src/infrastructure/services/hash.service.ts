b118e82cb2d71f80f3a5a2f5bcb375ac86a91c1a
shafeeque <shafeeqsha06@gmail.com>
2025-05-20 13:29:40 +0530

    feat(hash-srv): add hash service contract

---
+++ src/infrastructure/services/hash.service.ts
@@ -0,0 +1,16 @@
+import IHashService from '@/application/services/hash.service';
+import bcrypt from 'bcrypt';
+import { injectable } from 'inversify';
+
+@injectable()
+export default class HashServiceImpl implements IHashService {
+  public readonly SALT_VALUE = 10;
+
+  public async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, this.SALT_VALUE);
+  }
+
+  public async compare(oldPassword: string, newPassword: string): Promise<boolean> {
+    return await bcrypt.compare(newPassword, oldPassword);
+  }
+}
