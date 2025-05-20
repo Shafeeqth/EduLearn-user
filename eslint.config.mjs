76e4385861a2478ac47273454e3ce5a0cbecdb06
shafeeque <shafeeqsha06@gmail.com>
2025-05-20 14:02:21 +0530

    chore: modified config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,27 +15,33 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['node_modules/', 'dist/', 'coverage/'],
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'coverage/',
+      'src/infrastructure/frameworks/gRPC/generated/*',
+      'protogen.sh'
+    ],
   },
   ...compat.extends(
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'eslint-config-prettier' // Ensure this is the last extended configuration
+    'eslint-config-prettier'
   ),
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.{ts,tsx}', '**/*.{ts,tsx}/**'],
     plugins: {
       '@typescript-eslint': typescript,
       prettier: prettier,
     },
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/explicit-member-accessibility': ['warn'],
       '@typescript-eslint/no-non-null-assertion': 'off',
-      'prettier/prettier': 'error', // Add Prettier as an ESLint rule
+      'prettier/prettier': 'error',
     },
   },
 ];
