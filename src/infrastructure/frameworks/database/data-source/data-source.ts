229053dab1ac33eee0a0dd31356ec67254c0a8c9
shafeeque <shafeeqsha06@gmail.com>
2025-05-24 23:22:28 +0530

    fix: change entities path to consider both .js and .ts exetensions

--- src/infrastructure/frameworks/database/data-source/data-source.ts
+++ src/infrastructure/frameworks/database/data-source/data-source.ts
@@ -1,15 +1,16 @@
 import { DataSource } from 'typeorm';
-import { getEnvs } from '@mdshafeeq-repo/edulearn-common';
+// import { getEnvs } from '@mdshafeeq-repo/edulearn-common';
 import path from 'path';
 
-const { DATABASE_URL } = getEnvs('DATABASE_URL');
+// const { DATABASE_URL } = getEnvs('DATABASE_URL');
+const DATABASE_URL = process.env.DATABASE_URL;
 
 export const AppDataSource = new DataSource({
   type: 'postgres', // Specifies the database type
   url: DATABASE_URL, // Database connection URL
   synchronize: process.env.NODE_ENV !== 'production', // Disable synchronization in production
-  entities: [path.resolve(__dirname, '../entities/*.ts')], // Paths to the entity files
-  migrations: [path.resolve(__dirname, '../migrations/*.ts')], // Paths to the migration files
+  entities: [path.resolve(__dirname, '../entities/*.{ts,js}')], // Paths to the entity files
+  migrations: [path.resolve(__dirname, '../migrations/*.{ts,js}')], // Paths to the migration files
   logging: ['error', 'migration'], // Enables logging for errors and migrations
   cache: {
     duration: 30000, // Cache duration in milliseconds (30 seconds)
@@ -19,8 +20,6 @@ export const AppDataSource = new DataSource({
     min: 5, // minimum number of connections in the pool
     idleTimeoutMillis: 30000, // close idle connections after 30 seconds
     connectionTimeoutMillis: 4000, // return an error after 4 seconds if connection could not be established
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   },
 });
